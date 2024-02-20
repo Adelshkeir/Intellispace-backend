98f217df5e9978a669325e423de19845bf264707
Adelshkeir <adelshkeir12@gmail.com>
2024-02-20 16:56:03 +0200

    solved a small problem with the products route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,15 +3,16 @@ import ProductsController from "../controllers/productControllers.js";
 import upload from "../middlewares/multer.js";
 const productRouter = express.Router();
 
-productRouter.post("/", upload.single("image"),ProductsController.createProduct);
+productRouter.post(
+  "/",
+  upload.single("image"),
+  ProductsController.createProduct
+);
 
 productRouter.get("/", ProductsController.getAllProducts);
 
+productRouter.get("/category/:categoryName", ProductsController.getProductsByCategory);
 productRouter.get("/curators_pick", ProductsController.getCuratorsPickProducts);
-
-productRouter.get("/:categoryName", ProductsController.getProductsByCategory);
-
-
 
 productRouter.delete("/:id", ProductsController.deleteProduct);
 
