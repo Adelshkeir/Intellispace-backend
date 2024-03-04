aa2e77f35a3ff1c9d50bf85ee086333c717284b0
Adelshkeir <adelshkeir12@gmail.com>
2024-03-04 10:20:02 +0200

    mission failed succefully

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -3,7 +3,6 @@ import Product from "../models/productModel.js";
 import Category from "../models/categoryModel.js";
 import Review from "../models/reviewModel.js";
 import User from "../models/userModel.js";
-import { v2 as cloudinary } from 'cloudinary';
 class ProductsController {
   static async createProduct(req, res) {
     try {
@@ -19,40 +18,25 @@ class ProductsController {
         return res.status(400).json({ errors });
       }
 
-      // Configure Cloudinary with API key
-      cloudinary.config({
-        cloud_name: 'dndveiiam',
-        api_key: '438283895288339',
-        api_secret: '7t00ka7usBz8lKwXDxdApIfDaCw',
-        secure: true,
-      });
+      try {
+        const newProduct = await Product.create({
+          name,
+          price,
+          description,
+          image: image.path,
+          curators_pick,
+          categoryId,
+        });
 
-      // Upload image to Cloudinary
-      cloudinary.uploader.upload_stream({ folder: 'intelispace' }, async (error, result) => {
-        if (error) {
-          return res.status(500).json({ error: 'Image upload failed' });
+        if (!newProduct) {
+          errors.push("Error creating product");
+          return res.status(500).json({ errors });
         }
 
-        try {
-          const newProduct = await Product.create({
-            name,
-            price,
-            description,
-            image: result.secure_url, // Use the secure URL provided by Cloudinary
-            curators_pick,
-            categoryId,
-          });
-
-          if (!newProduct) {
-            errors.push("Error creating product");
-            return res.status(500).json({ errors });
-          }
-
-          return res.status(201).json({ newProduct });
-        } catch (sequelizeError) {
-          return res.status(500).json({ message: sequelizeError.message });
-        }
-      }).end(image.buffer); // Assuming the uploaded image data is in req.file.buffer
+        return res.status(201).json({ newProduct });
+      } catch (sequelizeError) {
+        return res.status(500).json({ message: sequelizeError.message });
+      }
     } catch (error) {
       return res.status(500).json({ message: error.message });
     }
